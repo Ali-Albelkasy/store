02db9e112d6606b9954a32554e2b6dc88726e824
Ali-Albelkasy <albelkasy697901@gmail.com>
2023-12-19 03:47:22 +0200

    khjgikj

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express()
 const port = 3000
 app.use(express.json())
 app.use(require('./router/app.routes'))
-mongoose.connect('mongodb+srv://alimostafa016979:WcJSejTQC0CVIu2J@cluster0.o6aqivx.mongodb.net/').then(() => {
+mongoose.connect('mongodb+srv://alimostafa016979:WcJSejTQC0CVIu2J@cluster0.o6aqivx.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
     console.log('connected');
 })
-app.listen(process.env.PORT||port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
